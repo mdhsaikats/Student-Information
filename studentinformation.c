ba4d9d2d800ef1c178886b729bd76ca4d9b87cdb
Saikat Sikder <45892103+mdhsaikats@users.noreply.github.com>
2024-01-25 16:44:14 +0600

    Update studentinformation.c

--- studentinformation.c
+++ studentinformation.c
@@ -8,7 +8,6 @@ struct info
 };
 void studentInformation(struct info student[10], int i)
 {
-
     if (i<10)
     {
         printf("Enter Student %d:\n",i + 1);
@@ -23,10 +22,8 @@ void studentInformation(struct info student[10], int i)
         printf("-----------------------------\n");
         printf("Maximum Student limit reached\n");
         printf("-----------------------------\n");
-    }
-    
+    } 
 }
-
 void studentList( struct info student[10],int n)
 {
     if (n > 0)
@@ -35,7 +32,6 @@ void studentList( struct info student[10],int n)
     {
         printf("Name: %s\nID: %d\nCGPA: %.2f\n",student[i].name , student[i].id , student[i].cgpa);
         printf("------------\n");
-
     }
     }else{
         printf("---------------------------------\n");
@@ -48,70 +44,52 @@ void individualName(struct info student[10], int n)
     char studname[50];
     printf("Student Name:");
     scanf("%s",&studname);
-
     for (int i = 0; i < n; i++)
     {
         if (strcmp(studname, student[i].name) == 0){
             printf("Name: %s\nID: %d\nCGPA: %.2f\n",student[i].name,student[i].id,student[i].cgpa);
             printf("------------\n");
             return;
-        
         }
     } 
         printf("-------------------\n");
         printf("No student found\n");
         printf("-------------------\n");
-
 }
-
 int main()
 {
     struct info student[10];
     int choice , i=0;
-
     while (1)
     {
         printf("1.Enter student information\n2.Show student list\n3.Search individual student\n4.Exit\n");
         printf("Enter your choice: ");
         scanf("%d",&choice);
         printf("---------------------\n");
-
         switch (choice)
         {
         case 1:
-
             studentInformation(student , i);
             i++;
-            
             break;
         case 2:
             studentList(student , i); 
-
             break;
-
         case 3:
-
             individualName(student , i);
-
             break;
-
         case 4:
             printf("*Thank You* :-)\n");
             printf("---------------------\n");
             return 0;
-
             break;
-
         default:
             printf("----------\n");
             printf("Error\n");
             printf("----------\n");
-
             break;
         }
-    }
-    
-   
+    } 
 }
 
 //saikat
